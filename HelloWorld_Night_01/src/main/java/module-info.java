43788320c8cb7ec376745b0cfe2b81015c82a0e5
Rayhan Amin <rayhanamin.lm2001@gmail.com>
2024-06-30 21:26:18 +0600

    Delete HelloWorld_Night_01 directory

--- HelloWorld_Night_01/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.oop.practicearea_2220972.helloworld_night_01 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.oop.practicearea_2220972.helloworld_night_01 to javafx.fxml;
-    exports com.oop.practicearea_2220972.helloworld_night_01;
-}
